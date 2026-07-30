41dbbff067496665b9b1444923053294c3f08daa
nisrinaazhaar <143578434+nisrinaazhaar@users.noreply.github.com>
2026-07-30 15:09:25 +0700

    Initialize Supabase client in search_articles function
    Added client creation for Supabase to the search_articles function.

--- search_article.py
+++ search_article.py
@@ -5,6 +5,7 @@ def search_articles(keyword=""):
 
     url = "https://jjmxqradekqclhbteplw.supabase.co"
     key = "sb_publishable_InNUNhB1KvsnsGhxJET8cQ_iUHX8uUg"
+supabase = create_client(url, key)
 
    response = (
         supabase
