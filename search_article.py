8e3d905a6448a1861015e27b0f72f4a6229d1c5d
nisrinaazhaar <143578434+nisrinaazhaar@users.noreply.github.com>
2026-07-30 15:10:21 +0700

    Refactor search_articles function
    Removed unnecessary connection close statement.

--- search_article.py
+++ search_article.py
@@ -7,17 +7,15 @@ def search_articles(keyword=""):
     key = "sb_publishable_InNUNhB1KvsnsGhxJET8cQ_iUHX8uUg"
 supabase = create_client(url, key)
 
-   response = (
-        supabase
-        .table("articles")
-        .select("*")
-        .or_(f"title.ilike.%{keyword}%,label.ilike.%{keyword}%")
-        .order("upload_time", desc=True)
-        .execute()
-    )
+ response = (
+     supabase
+     .table("articles")
+     .select("*")
+     .or_(f"title.ilike.%{keyword}%,label.ilike.%{keyword}%")
+     .order("upload_time", desc=True)
+     .execute()
+ )
     
     df = pd.DataFrame(response.data)
-
-    conn.close()
 
     return df
