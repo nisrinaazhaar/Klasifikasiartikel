87eb54fede79c2331b88a3a57105f9e87e62c725
nisrinaazhaar <143578434+nisrinaazhaar@users.noreply.github.com>
2026-07-30 15:11:25 +0700

    Enhance search_articles function with keyword handling
    Added conditional logic to handle cases with and without a keyword.

--- search_article.py
+++ search_article.py
@@ -1,21 +1,35 @@
 from supabase import create_client
 import pandas as pd
 
+
 def search_articles(keyword=""):
 
     url = "https://jjmxqradekqclhbteplw.supabase.co"
     key = "sb_publishable_InNUNhB1KvsnsGhxJET8cQ_iUHX8uUg"
-supabase = create_client(url, key)
 
- response = (
-     supabase
-     .table("articles")
-     .select("*")
-     .or_(f"title.ilike.%{keyword}%,label.ilike.%{keyword}%")
-     .order("upload_time", desc=True)
-     .execute()
- )
-    
+    supabase = create_client(url, key)
+
+    if keyword:
+
+        response = (
+            supabase
+            .table("articles")
+            .select("*")
+            .or_(f"title.ilike.%{keyword}%,label.ilike.%{keyword}%")
+            .order("upload_time", desc=True)
+            .execute()
+        )
+
+    else:
+
+        response = (
+            supabase
+            .table("articles")
+            .select("*")
+            .order("upload_time", desc=True)
+            .execute()
+        )
+
     df = pd.DataFrame(response.data)
 
     return df
